698574e3b092fb2f006c2346cdfc762301d26bce
Uzzal Kar <uzzalkar12@gmail.com>
2019-10-01 15:39:07 +0600

    set up dashboard

---
+++ resources/views/dashboard/layout.blade.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Dashboard V.1 | Kiaalap - Kiaalap Admin Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+		============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <!-- Google Fonts
+		============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.theme.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.transitions.css')}}">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/normalize.css')}}">
+    <!-- meanmenu icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/meanmenu.min.css')}}">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+    <!-- educate icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/educate-custon-icon.css')}}">
+    <!-- morrisjs CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/morrisjs/morris.css')}}">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/scrollbar/jquery.mCustomScrollbar.min.css')}}">
+    <!-- metisMenu CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/metisMenu/metisMenu.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/metisMenu/metisMenu-vertical.css')}}">
+    <!-- calendar CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/calendar/fullcalendar.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/calendar/fullcalendar.print.min.css')}}">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/style.css')}}">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="{{asset('assets/js/vendor/modernizr-2.8.3.min.js')}}"></script>
+</head>
+
+<body>
+    @yield('section')
+
+    <!-- jquery
+		============================================ -->x
+    <script src="{{asset('assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="{{asset('assets/js/wow.min.js')}}"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="{{asset('assets/js/jquery-price-slider.js')}}"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="{{asset('assets/js/jquery.meanmenu.js')}}"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+    <!-- sticky JS
+		============================================ -->
+    <script src="{{asset('assets/js/jquery.sticky.js')}}"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="{{asset('assets/js/jquery.scrollUp.min.js')}}"></script>
+    <!-- counterup JS
+		============================================ -->
+    <script src="{{asset('assets/js/counterup/jquery.counterup.min.js')}}"></script>
+    <script src="{{asset('assets/js/counterup/waypoints.min.js')}}"></script>
+    <script src="{{asset('assets/js/counterup/counterup-active.js')}}"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="{{asset('assets/js/scrollbar/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <script src="{{asset('assets/js/scrollbar/mCustomScrollbar-active.js')}}"></script>
+    <!-- metisMenu JS
+		============================================ -->
+    <script src="{{asset('assets/js/metisMenu/metisMenu.min.js')}}"></script>
+    <script src="{{asset('assets/js/metisMenu/metisMenu-active.js')}}"></script>
+    <!-- morrisjs JS
+		============================================ -->
+    <script src="{{asset('assets/js/morrisjs/raphael-min.js')}}"></script>
+    <script src="{{asset('assets/js/morrisjs/morris.js')}}"></script>
+    <script src="{{asset('assets/js/morrisjs/morris-active.js')}}"></script>
+    <!-- morrisjs JS
+		============================================ -->
+    <script src="{{asset('assets/js/sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{asset('assets/js/sparkline/jquery.charts-sparkline.js')}}"></script>
+    <script src="{{asset('assets/js/sparkline/sparkline-active.js')}}"></script>
+    <!-- calendar JS
+		============================================ -->
+    <script src="{{asset('assets/js/calendar/moment.min.js')}}"></script>
+    <script src="{{asset('assets/js/calendar/fullcalendar.min.js')}}"></script>
+    <script src="{{asset('assets/js/calendar/fullcalendar-active.js')}}"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="{{asset('assets/js/plugins.js')}}"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="{{asset('assets/js/main.js')}}"></script>
+    <!-- tawk chat JS
+		============================================ -->
+    <script src="{{asset('assets/js/tawk-chat.js')}}"></script>
+</body>
+
+</html>
